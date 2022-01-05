8f75996c5f21f0f193b2a34e876db5ec49076cf1
Devon <Devon.Hartlen@uwaterloo.ca>
2022-01-05 15:30:42 -0500

    Functionized
    Moved S-G Filter to independent function. Added separate driver for testing.

---
+++ SGFilter.m
@@ -0,0 +1,60 @@
+function yyFilt = SGFilter(yy, k, n , s, varargin)
+
+
+yyFilt = zeros(size(yy));
+m = (n-1)/2;
+for iPt = 1:length(yy)
+    if iPt <= m
+        t = iPt-m-1;
+        weights = convWeights(k,s,t,n);
+        yyFilt(iPt) = sum(yy(iPt-m-t:iPt+m-t).*weights);
+    elseif iPt > length(yy)-m
+        t = (iPt-length(yy))+m;
+        weights = convWeights(k,s,t,n);
+        yyFilt(iPt) = sum(yy(iPt-m-t:iPt+m-t).*weights);
+    else
+        weights = convWeights(k,s,0,n);
+        yyFilt(iPt) = sum(yy(iPt-m:iPt+m).*weights);
+    end
+end
+
+end
+
+
+function weights = convWeights(k, s, t, winSize)
+m = (winSize-1)/2;
+weights = zeros(winSize,1);
+for iIter = 1:winSize
+    i = iIter-m-1;
+    weights(iIter) = pointWeight(i, t, m, k, s);
+end
+
+end
+
+function out = gramPolynomial(i, m, k, s)
+if k > 0
+    out = (4*k-2)/(k*(2*m-k+1))*(i*gramPolynomial(i, m, k-1, s) + s* ...
+        gramPolynomial(i, m, k-1, s-1)) - ((k-1)*(2*m+k))/(k*(2*m-k+1))*...
+        gramPolynomial(i, m, k-2, s);
+elseif ((k==0) && (s==0))
+    out = 1;
+else
+    out = 0;
+end
+end
+
+function out = factors(a, b)
+out = 1;
+for iIter = (a-b+1):a
+    out = out*iIter;
+end
+end
+
+function weight = pointWeight(i, t, m, n, s)
+weight = 0;
+for k = 0:n
+    weight = weight + (2*k+1)*(factors(2*m, k) / ...
+        factors(2*m+k+1, k+1)) * gramPolynomial(i, m, k, 0) * ...
+        gramPolynomial(t, m, k, s);
+end
+end
---
+++ SGFilterDriver.m
@@ -0,0 +1,37 @@
+fclose all;
+close all;
+clear;
+clc;
+
+cset = cbrewer2('set1',4)
+colormap(cset)
+
+k = 5; % polynomial order
+winSize = 25; % window size (must be odd)
+s = 0; % differentiation order
+
+%% Fake experimental data
+nPts = 500
+xx = linspace(0,4,nPts)';
+dx = (xx(end)-xx(1))./nPts;
+yyOg = cos(pi.*xx);
+yyOgDiff = -pi.*sin(pi.*xx);
+
+rng(128493)
+noiseFact = 0.5
+yy = yyOg + noiseFact * (0.5-rand(nPts,1));
+
+figure();
+subplot(1,2,1); hold on; 
+plot(xx,yyOg, 'DisplayName','Ground Truth','color',cset(1,:))
+plot(xx,yy, 'DisplayName','Added Noise','color',cset(2,:))
+subplot(1,2,2); hold on;
+plot(xx,yyOgDiff, 'DisplayName','Ground Truth','color',cset(1,:))
+plot(xx(1:end-1)+0.5*dx,diff(yy)./dx, 'DisplayName','Added Noise','color',cset(2,:))
+
+yyFilt = SGFilter(yy,k, winSize,s);
+subplot(1,2,1)
+plot(xx,yyFilt, 'DisplayName','S-G Filter','color',cset(3,:))
+legend()
+
+yyDerivFilt 
